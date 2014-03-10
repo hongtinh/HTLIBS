a7ab4f1e3a934783418656ef82e253bf43b986e7
haohongting <haohongting@gmail.com>
2014-03-10 21:21:26 +0800

    add merge sort

--- algrithms/sort.c
+++ algrithms/sort.c
@@ -38,3 +38,40 @@ void quickSort(int* data, int low, int high)
 
 
 
+/* for merge sort */
+void merge(int *data, int low, int mid, int high)
+{
+  int i, j, k;
+  int *pTmp = (int*)malloc( sizeof(int) * (high - low + 1) );
+
+  for( i = low, j = mid + 1, k = 0; i <= mid; k++){
+    if( j > high) break;
+    if( *(data + i) <= *(data + j) ){
+      *(pTmp + k) = *(data + i);
+      i++;
+    }else{
+      *(pTmp + k) = *(data + j);
+      j++;
+    }
+  }
+
+  if( i <= mid ){
+    memcpy( pTmp + k, data + i, sizeof(int)*(mid-i + 1));
+  }
+  if( j <= high){
+    memcpy( pTmp + k, data + j, sizeof(int)*(high-j +1));
+  }
+  memcpy(data + low, pTmp, sizeof(int)*(high - low +1));
+  if(pTmp) free(pTmp);
+}
+
+
+void mergeSort(int *data, int low, int high)
+{
+  if( low < high){
+    int mid = (low + high)/2;
+    mergeSort(data, low, mid);
+    mergeSort(data, mid + 1, high);
+    merge(data, low, mid, high);
+  }
+}
